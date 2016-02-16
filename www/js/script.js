7482b7c4375d48d0b887115d54b5a16d922ec6ed
Theo Hinfray <hinfraytheo@gmail.com>
2016-02-16 17:17:41 +0100

    système de changement de pages

---
+++ www/js/script.js
@@ -0,0 +1,15 @@
+/* système affichage  */ 
+
+function qsdf(elem){
+
+
+	
+	document.getElementById("accueil").style.display= "none";
+	document.getElementById("postSeul").style.display= "none";
+	document.getElementById("posts").style.display= "none";
+	document.getElementById("compte").style.display= "none";
+	document.getElementById(elem).style.display= "block";
+
+
+
+}
